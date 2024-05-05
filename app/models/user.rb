6c4c215c205cd9910f5c3b92b2b22035c187e709
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-05 02:37:28 +0000

    The automatic logout feature has been disabled.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :timeoutable
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
