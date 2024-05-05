63b0da530867cb25654b1288cdc253d4066a1e1d
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-05 02:32:52 +0000

    Added setting to automatically log out after 1 hour.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
