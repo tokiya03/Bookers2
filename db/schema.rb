3532dd893a57ab3357eb7c047139ab0dcc37fbe5
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-05-07 07:50:20 +0000

    Create a model for groups and group users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_17_052342) do
+ActiveRecord::Schema.define(version: 2024_05_07_074824) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,22 @@ ActiveRecord::Schema.define(version: 2024_04_17_052342) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "introducion"
+    t.integer "image_id"
+    t.integer "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
